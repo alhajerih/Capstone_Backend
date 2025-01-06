1f123778e70c6b128737c4f3385cc865090748a0
alhajerih <alhajerih@hotmail.com>
2025-01-06 08:07:50 +0300

    APIGatway+eurekaServer

---
+++ AuthService/src/main/java/com/example/AuthService/bo/CreateUserRequest.java
@@ -0,0 +1,28 @@
+package com.example.AuthService.bo;
+
+public class CreateUserRequest {
+
+    private String username;
+    private String password;
+
+    public CreateUserRequest() {
+        //Default constructor
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ AuthService/src/main/java/com/example/AuthService/bo/auth/LogoutResponse.java
@@ -0,0 +1,15 @@
+package com.example.AuthService.bo.auth;
+
+
+public class LogoutResponse {
+
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
