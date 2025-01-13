61bda8322025522623061f2084afd8465fff1ba6
alhajerih <alhajerih@hotmail.com>
2025-01-13 10:20:01 +0300

    swagger

--- Shares/src/main/java/com/example/Shares/config/SecurityConfig.java
+++ Shares/src/main/java/com/example/Shares/config/SecurityConfig.java
@@ -138,6 +138,13 @@ public class SecurityConfig extends org.springframework.security.config.annotati
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
+                .antMatchers(
+                        "/swagger-ui.html",
+                        "/swagger-ui/**",
+                        "/v3/api-docs",
+                        "/swagger-resources/**",
+                        "/webjars/**"
+                ).permitAll()
                 .antMatchers(AUTH_PATH+"/login", AUTH_PATH+"/generate-otp", AUTH_PATH+"/validate-otp", AUTH_PATH+"/register").permitAll()
                 .anyRequest().authenticated()
                 .and()
