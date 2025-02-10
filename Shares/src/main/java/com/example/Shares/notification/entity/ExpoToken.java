3c608ce24e4b9ae4e15210fa8d006af98dec6c28
alhajerih <alhajerih@hotmail.com>
2025-02-10 23:40:18 +0300

    notification microservice

--- Shares/src/main/java/com/example/Shares/notification/entity/ExpoToken.java
+++ Shares/src/main/java/com/example/Shares/notification/entity/ExpoToken.java
@@ -1,47 +1,47 @@
-package com.example.Shares.notification.entity;
-
-import com.example.Shares.auth.entity.UserEntity;
-import com.fasterxml.jackson.annotation.JsonBackReference;
-
-import javax.persistence.*;
-
-@Entity
-public class ExpoToken {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(unique = true) // each token should be unique
-    private String token;
-
-    @OneToOne
-    @JoinColumn(name = "user_id", nullable = false)
-    @JsonBackReference
-    private UserEntity user;
-    // Getter and Setter
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public UserEntity getUser() {
-        return user;
-    }
-
-    public void setUser(UserEntity user) {
-        this.user = user;
-    }
-}
+//package com.example.Shares.notification.entity;
+//
+//import com.example.Shares.auth.entity.UserEntity;
+//import com.fasterxml.jackson.annotation.JsonBackReference;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//public class ExpoToken {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Column(unique = true) // each token should be unique
+//    private String token;
+//
+//    @OneToOne
+//    @JoinColumn(name = "user_id", nullable = false)
+//    @JsonBackReference
+//    private UserEntity user;
+//    // Getter and Setter
+//
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getToken() {
+//        return token;
+//    }
+//
+//    public void setToken(String token) {
+//        this.token = token;
+//    }
+//
+//    public UserEntity getUser() {
+//        return user;
+//    }
+//
+//    public void setUser(UserEntity user) {
+//        this.user = user;
+//    }
+//}
--- notificationservice/src/test/java/com/example/notificationservice/NotificationserviceApplicationTests.java
+++ notificationservice/src/test/java/com/example/notificationservice/NotificationserviceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.AuthService;
+package com.example.notificationservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AuthServiceApplicationTests {
+class NotificationserviceApplicationTests {
 
 	@Test
 	void contextLoads() {
