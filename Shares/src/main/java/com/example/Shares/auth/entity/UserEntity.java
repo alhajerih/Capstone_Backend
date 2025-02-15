fc1b73209fbfba108d021ade41e9cfccb5ac5db6
Yousef Almesaeed <yousefalm@Outlook.com>
2025-02-15 16:13:26 +0300

    Add new Seeder and add firstname and lastname

--- Shares/src/main/java/com/example/Shares/auth/entity/UserEntity.java
+++ Shares/src/main/java/com/example/Shares/auth/entity/UserEntity.java
@@ -19,6 +19,8 @@ public class UserEntity implements Serializable {
     private Long id;
 
     private String civilId;
+    private String firstName;
+    private String lastName;
     private String username;
     private String password;
     private String phoneNumber;
@@ -54,6 +56,22 @@ public class UserEntity implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getCivilId() {
