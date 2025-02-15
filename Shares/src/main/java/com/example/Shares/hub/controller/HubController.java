f88f82dbbe42189528c73bdc9602a417ff0c59f9
alhajerih <alhajerih@hotmail.com>
2025-02-15 04:06:50 +0300

    page

--- Shares/src/main/java/com/example/Shares/hub/controller/HubController.java
+++ Shares/src/main/java/com/example/Shares/hub/controller/HubController.java
@@ -145,7 +145,7 @@ public class HubController {
     }
 
 
-    @GetMapping("/Hub-transactions")
+    @GetMapping("/hub-transactions")
     public ResponseEntity<?> getTransactionsForHub(@RequestHeader("Authorization") String token,@RequestParam Long hubId,@RequestParam(defaultValue = "0") int page,@RequestParam(defaultValue = "10") int size) {
 
         String jwt = token.substring(7);
