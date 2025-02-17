b4f3d5f3c365fbcc49412ae9aeed93118267dbc5
alhajerih <alhajerih@hotmail.com>
2025-02-18 00:22:51 +0300

    Project is ready

--- Shares/src/main/java/com/example/Shares/auth/repository/BankCardRepository.java
+++ Shares/src/main/java/com/example/Shares/auth/repository/BankCardRepository.java
@@ -1,6 +1,7 @@
 package com.example.Shares.auth.repository;
 
 import com.example.Shares.auth.entity.BankCardEntity;
+import com.example.Shares.auth.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,5 +10,6 @@ import java.util.Optional;
 public interface BankCardRepository extends JpaRepository<BankCardEntity, Long> {
 
     Optional<BankCardEntity> findByCardNumber(String cardNumber);
+    List<BankCardEntity> findByUser(UserEntity user);
 
 }
